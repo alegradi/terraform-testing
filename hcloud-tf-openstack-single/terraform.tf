f92eddbc25eb07c18f21586a8eeab62bfbdaec80
Adam Legradi <adam.legradi@protonmail.ch>
2019-11-19 17:55:34 +0100

    Initial commit for openstack single node Terraform

---
+++ hcloud-tf-openstack-single/terraform.tf
@@ -0,0 +1,57 @@
+# https://medium.com/@gtrafimenkov/how-to-manage-hetzner-cloud-with-terraform-25308fab6c22
+
+## Variables come here ##
+variable "hcloud_token" {}
+
+# Provider
+provider "hcloud" {
+  token 	= var.hcloud_token
+}
+
+# SSH key
+data "hcloud_ssh_key" "centos7-terraform_key" {
+  id		= "1121792"
+}
+
+# Node 1 configuration
+resource "hcloud_server" "openstacksinglenode" {
+  name        	= "openstacksinglenode"
+  image       	= "centos-7"
+  server_type 	= "cx21"
+  ssh_keys    	= ["${data.hcloud_ssh_key.centos7-terraform_key.id}"]
+
+  provisioner "remote-exec" {
+    inline = [
+      "export PATH=$PATH:/usr/bin",
+      "yum update -y"
+    ]
+  }
+
+}
+
+
+
+# Private netwok configuration - needs further editing
+resource "hcloud_network" "testnetwork" {
+  name 		= "test-network"
+  ip_range 	= "10.0.0.0/8"
+}
+
+resource "hcloud_network_subnet" "openstack_vlan" {
+  network_id 	= hcloud_network.testnetwork.id
+  network_zone 	= "eu-central"
+  type 		= "server"
+  ip_range   	= "10.0.1.0/24"
+}
+
+resource "hcloud_server_network" "srvnetwork1" {
+  server_id 	= hcloud_server.openstacksinglenode.id
+  network_id 	= hcloud_network.testnetwork.id
+  ip 		= "10.0.1.5"
+}
+
+
+output "public_ip4_node1" {
+  value 	= "${hcloud_server.openstacksinglenode.ipv4_address}"
+}
+
